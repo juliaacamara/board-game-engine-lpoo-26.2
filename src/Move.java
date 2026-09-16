b52edfccc74e7aaa94a35bdf7d52f9c19b10134f
juliaacamara <julia.camara@upe.br>
2026-09-16 09:50:57 -0300

    Feature: Add Board class, Game class, Main class, Refactor other classes.

--- src/Move.java
+++ src/Move.java
@@ -1,5 +1,3 @@
-import java.util.Objects;
-
 public class Move {
     private Player player;
     private Position target;
@@ -17,23 +15,4 @@ public class Move {
         return this.target;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null || this.getClass() != obj.getClass())
-            return false;
-        Move other = (Move) obj;
-        return Objects.equals(this.player, other.player) && Objects.equals(this.target, other.target);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.player, this.target);
-    }
-
-    @Override
-    public String toString() {
-        return this.player.getSymbol() + " -> " + this.target;
-    }
 }
